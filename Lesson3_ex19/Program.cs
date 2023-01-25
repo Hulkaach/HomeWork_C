3a187c20d104a07850e36d3f3389270347b79482
Anton <hulkaach@mail.ru>
2023-01-25 20:15:44 +0300

    third work done

--- Lesson3_ex19/Program.cs
+++ Lesson3_ex19/Program.cs
@@ -3,7 +3,18 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
-Console.WriteLine("Введите число ");
+Console.Write("Введите пятизначное число: ");
 int num = int.Parse(Console.ReadLine());
+int num1 = num / 10000;
+int num2 = num % 10;
+int num3 = (num / 1000) % 10;
+int num4 = (num / 10) % 10;
 
-Console.WriteLine("Введенное число " + num + " является палиндромом")
+if (num1 == num2 && num3 == num4)
+{
+    Console.WriteLine("Введенное число " + num + " является палиндромом");
+}
+else 
+{
+    Console.WriteLine("Введенное число " + num + " не является палиндромом");
+}  
