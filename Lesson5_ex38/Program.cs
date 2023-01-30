e9a5e2de16eff53250d7d7fb7ad51967d7119b9c
Anton <hulkaach@mail.ru>
2023-01-30 22:37:58 +0300

    Third work done so-so

--- Lesson5_ex38/Program.cs
+++ Lesson5_ex38/Program.cs
@@ -1,3 +1,26 @@
 ﻿// Задача 38: Задайте массив целых чисел. Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
-Console.WriteLine("Hello, World!");
+int rand = new Random().Next(1, 10);
+int[] array = new int[rand];
+void FillArray()
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+        Console.Write($"{array[i]} ");
+    }
+
+    Console.Write($"\b] -> ");
+
+}
+FillArray();
+
+int min = array[0];
+int max = array[0];
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] < min) min = array[i];
+    if (array[i] > max) max = array[i];
+}
+Console.Write(max - min);
