957299c5cc55974e856104690cc1aeb1c6195c8d
unknown <hulkaach@mail.ru>
2023-02-09 22:56:46 +0300

    four work done

--- Lesson8_ex60/Program.cs
+++ Lesson8_ex60/Program.cs
@@ -4,3 +4,44 @@
 // 34(1,0,0) 41(1,1,0)
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
+Console.WriteLine("Введите размерность трехмерной матрицы: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+int columns = rows;
+int depth = rows;
+int[,,] array = new int[rows, columns, depth];
+
+Console.WriteLine("Матрица: ");
+FillArray(array);
+PrintArray(array);
+
+void FillArray(int[,,] argyment)
+{
+    int temp = 10;
+    for (int i = 0; i < argyment.GetLength(0); i++)
+    {
+        for (int j = 0; j < argyment.GetLength(1); j++)
+        {
+            for (int k = 0; k < argyment.GetLength(2); k++)
+            {
+
+                argyment[i, j, k] = temp;
+                temp++;
+            }
+        }
+    }
+}
+
+void PrintArray(int[,,] argyment)
+{
+    for (int i = 0; i < argyment.GetLength(0); i++)
+    {
+        for (int j = 0; j < argyment.GetLength(1); j++)
+        {
+            for (int k = 0; k < argyment.GetLength(2); k++)
+            {
+                Console.Write($"{argyment[i, j, k],4} ({i},{j},{k})");
+            }
+            Console.WriteLine();
+        }
+    }
+}
