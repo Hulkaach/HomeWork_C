f675d2984d1317670fc4d568b7fd5caa52cadd3f
unknown <hulkaach@mail.ru>
2023-02-11 22:00:13 +0300

    Lesson 9 done

---
+++ Lesson9_ex66/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+Console.Write("Введите минимальное значение - M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное значение - N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(SumNumbers(m, n));
+
+int SumNumbers(int argument1, int argument2)
+{
+    if (argument1 == argument2) return argument2;
+    return (argument1 + SumNumbers(argument1 + 1, argument2));
+}
---
+++ Lesson9_ex68/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+Console.Write("Введите значение m функции Аккeрмана: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение n функции Аккeрмана: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(AkkermanFunction(min, max));
+
+int AkkermanFunction(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0 && m > 0) return AkkermanFunction(m - 1, 1);
+    else if (m > 0 && n > 0) return (AkkermanFunction(m - 1, AkkermanFunction(m, n - 1)));
+    return AkkermanFunction(m, n);
+}
